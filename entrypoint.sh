81178c1df69b617c436595f2e7b6834060f49fb6
Gopi C N <gopigowda132@gmail.com>
2025-12-01 22:22:59 +0530

    "fix"

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-if [ -z "$SPRING_DATASOURCE_URL" ] && [ ! -z "$DB_HOST" ]; then
+
+if [ ! -z "$SPRING_DATASOURCE_URL" ]; then
+  if [[ ! "$SPRING_DATASOURCE_URL" =~ ^jdbc: ]]; then
+    export SPRING_DATASOURCE_URL="jdbc:$SPRING_DATASOURCE_URL"
+  fi
+elif [ ! -z "$DB_HOST" ]; then
   export SPRING_DATASOURCE_URL="jdbc:postgresql://$DB_HOST:${DB_PORT:-5432}/$DB_NAME"
 fi
+
 exec java -jar app.jar --spring.profiles.active=prod
